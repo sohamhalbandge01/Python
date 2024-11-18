bb448b2927d53df46849715c3ee6ab5cc00996c8
Soham Halbandge <logzii123@gmail.com>
2024-11-19 00:52:57 +0530

    commit

---
+++ 04_Conditional_Statements/eligible_to_work.py
@@ -0,0 +1,6 @@
+age = int(input("Enter the age:"))
+
+if age >= 18 and age <= 60:
+    print("Eligible")
+else:
+    print("Not Eligible")
---
+++ 04_Conditional_Statements/valid_marks.py
@@ -0,0 +1,6 @@
+marks = int(input("Enter the marks:"))
+
+if marks >= 0 and marks <= 100:
+    print("Valid")
+else:
+    print("Invalid")

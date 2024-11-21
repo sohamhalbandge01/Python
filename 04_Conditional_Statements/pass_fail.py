9299b814f08fbb7956ccde95bbcc6d1c38e1c036
Soham Halbandge <logzii123@gmail.com>
2024-11-21 09:08:23 +0530

    commit

---
+++ 04_Conditional_Statements/pass_fail.py
@@ -0,0 +1,8 @@
+maths_marks = int(input("Enter the marks:"))
+physics_marks = int(input("Enter the marks:"))
+chem_marks = int(input("Enter the marks:"))
+
+if maths_marks >= 45 and physics_marks >= 45 and chem_marks >= 45:
+    print("PASS!")
+else:
+    print("FAIL!")
---
+++ 04_Conditional_Statements/vowel_check.py
@@ -0,0 +1,6 @@
+char = input("Enter the alphabet:")
+
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print("Vowel")
+else:
+    print("Consonant")
